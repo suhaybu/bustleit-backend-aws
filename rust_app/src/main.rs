7d61a62ccad1e916510981d7b56ea17f878677da
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-03 18:52:44 +0300

    now uses Axum, updated for new routes

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,43 +1,14 @@
-use lambda_runtime::{service_fn, Error, LambdaEvent};
-
-use serde::{Deserialize, Serialize};
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use lambda_http::{run, tracing, Error};
+use std::env::set_var;
 
 mod handlers;
 mod models;
 mod services;
 mod utils;
-
-/// This is a made-up example. Requests come into the runtime as unicode
-/// strings in json format, which can map to any structure that implements `serde::Deserialize`
-/// The runtime pays no attention to the contents of the request payload.
-#[derive(Deserialize)]
-struct Request {}
-
-/// This is a made-up example of what a response structure may look like.
-/// There is no restriction on what it can be. The runtime requires responses
-/// to be serialized into json. The runtime pays no attention
-/// to the contents of the response payload.
-#[derive(Serialize)]
-struct Response {
-    statusCode: i32,
-    body: String,
-}
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
-    // Prepare the response
-    let resp = Response {
-        statusCode: 200,
-        body: "Hello World!".to_string(),
-    };
-
-    // Return `Response` (it will be serialized to JSON automatically by the runtime)
-    Ok(resp)
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -49,5 +20,45 @@ async fn main() -> Result<(), Error> {
         .without_time()
         .init();
 
-    lambda_runtime::run(service_fn(function_handler)).await
+    // If you use API Gateway stages, the Rust Runtime will include the stage name
+    // as part of the path that your application receives.
+    // Setting the following environment variable, you can remove the stage from the path.
+    // This variable only applies to API Gateway stages,
+    // you can remove it if you don't use them.
+    // i.e with: `GET /test-stage/todo/id/123` without: `GET /todo/id/123`
+    set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
+
+    // Build the router
+    let app = Router::new()
+        // Auth routes
+        .route("/auth/login", post(handlers::auth::login))
+        // AI routes
+        .route("/ai/request/userdata", post(handlers::ai::get_userdata))
+        .route("/ai/request/allusers", get(handlers::ai::get_all_users))
+        .route(
+            "/ai/request/clusteredUsers",
+            get(handlers::ai::get_clustered_users),
+        )
+        .route("/ai/request/schedules", post(handlers::ai::get_schedules))
+        .route("/ai/request/tasks", get(handlers::ai::get_tasks))
+        .route(
+            "/ai/request/taskedUsers",
+            get(handlers::ai::get_tasked_users),
+        )
+        // Recommend routes
+        .route("/recommend", post(handlers::recommend::get_recommendation))
+        // .route(
+        //     "/recommend/test",
+        //     get(handlers::recommend::get_test_recommendation),
+        // )
+        // Profile routes
+        // .route(
+        //     "/profile",
+        //     get(handlers::profile::get_profile).post(handlers::profile::update_profile),
+        // )
+        // Health check
+        .route("/health", get(handlers::health::check));
+
+    // Run it with Lambda runtime
+    run(app).await
 }
