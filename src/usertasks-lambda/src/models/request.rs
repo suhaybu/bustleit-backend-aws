99166ed9a9535d34ee601e40b11f4cac5f2d95b1
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-20 20:46:37 +0300

    added structs for CreateTaskRequest, UpdateTaskRequest

--- src/usertasks-lambda/src/models/request.rs
+++ src/usertasks-lambda/src/models/request.rs
@@ -4,3 +4,22 @@ use serde::Deserialize;
 pub struct TasksRequest {
     pub user_ids: Vec<String>,
 }
+
+#[derive(Deserialize)]
+pub struct CreateTaskRequest {
+    pub name: String,
+    pub category: String,
+    pub start_time: String,
+    pub end_time: String,
+    pub date: String,
+}
+
+#[derive(Deserialize)]
+pub struct UpdateTaskRequest {
+    pub name: Option<String>,
+    pub category: Option<String>,
+    pub start_time: Option<String>,
+    pub end_time: Option<String>,
+    pub completed: Option<bool>,
+    pub date: Option<String>,
+}
