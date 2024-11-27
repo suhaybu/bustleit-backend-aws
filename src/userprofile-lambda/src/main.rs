e01146cd63643a8055f50621f92a15fe502adc1a
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-27 17:49:10 +0300

    renamed fn post_register_user -> create_user_profile

--- src/userprofile-lambda/src/main.rs
+++ src/userprofile-lambda/src/main.rs
@@ -24,7 +24,7 @@ async fn main() -> Result<(), Error> {
     set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
 
     let app = Router::new()
-        .route("/v1/signup", post(register::post_register_user))
+        .route("/v1/signup", post(register::create_user_profile))
         .route("/v1/user/profile/:id", get(profile::get_profile))
         .route("/v1/user/profiles", get(profiles::get_profiles))
         .route("/v1/user/profiles/batch", post(profiles::get_batch))
