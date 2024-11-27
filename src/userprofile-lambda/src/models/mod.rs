bba9a3bafe0acbcc485248292cdfbcab1c272d27
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-27 19:50:21 +0300

    added mod recommend

--- src/userprofile-lambda/src/models/mod.rs
+++ src/userprofile-lambda/src/models/mod.rs
@@ -1,6 +1,7 @@
-mod external;
 mod profiles;
+mod recommend;
 mod register;
 
 pub use profiles::{convert_profiles, UserProfile, UserProfilesBatchRequest, UserProfilesQuery};
+pub use recommend::{RequestRecommendDaily, ResponseRecommendDaily};
 pub use register::RegisterUserPayload;
