82d322cb8aba490c18477e75eb2188b06c0dafd8
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-28 13:26:45 +0300

    updated imports

--- src/userprofile-lambda/src/models/mod.rs
+++ src/userprofile-lambda/src/models/mod.rs
@@ -3,5 +3,7 @@ mod recommend;
 mod register;
 
 pub use profiles::{convert_profiles, UserProfile, UserProfilesBatchRequest, UserProfilesQuery};
-pub use recommend::{RequestRecommendDaily, ResponseRecommendDaily};
+pub use recommend::{
+    RequestRecommendDaily, RequestRecommendWeekly, ResponseRecommendDaily, ResponseRecommendWeekly,
+};
 pub use register::RegisterUserPayload;
